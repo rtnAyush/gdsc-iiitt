427444b4af5566df3a1d24490af03422e30d097a
Rishabh705 <rishabh0739@gmail.com>
2024-01-15 13:05:20 +0530

    Updated source code for hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,45 @@
 import Teams from "@/components/teams/Teams";
-import MaxWidthWrapper from "@/components/MaxWidthWrapper"
-import Image from 'next/image'
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 export default async function Page() {
-
   return (
     <>
-      <MaxWidthWrapper className='bg-slate-200 '>
-        <section className='py-16 bg-slate-200 flex flex-col gap-16 max-w-6xl m-auto px-3 lg:gap-0 lg:flex-row'>
-          <section className='flex flex-col gap-12 justify-around sm:w-full lg:gap-0 lg:w-2/3'>
-            <h1 className='text-4xl tracking-tight text-gray-900 text-center lg:text-left sm:text-6xl'>
-              Ignite <span className="">Collaboration</span>, <br className="block lg:hidden"/> 
-              Unlock <span className="">Innovation</span>
+      <MaxWidthWrapper className="bg-[url('../public/assets/background.png')] bg-cover relative">
+        <section className="py-20 flex flex-col relative gap-16 max-w-6xl m-auto px-3 lg:gap-0 lg:flex-row ">
+          <section className="flex flex-col pt-16 gap-8 justify-center sm:w-full lg:w-2/3 md:pt-0">
+            <p className="text-xl font-normal text-gray-700 text-center lg:text-left">
+              Welcome to the Google Developer Student Club (GDSC)
+            </p>
+            <h1 className="text-5xl leading-tight font-bold text-gray-900 text-center lg:text-left md:leading-tight sm:text-6xl">
+              Ignite <span className="text-red-500">Collaboration</span>,{" "}
+              <br className="block lg:hidden" />
+              Unlock <span className="text-green-500">Innovation</span>
             </h1>
-            <div className="flex max-w-xs mx-auto w-full lg:mx-0 lg:max-w-sm">
-              <Input className="rounded-r-none active:outline-none" placeholder="Email address"/>
-              <Button variant="default" className="rounded-l-none">Join Club</Button>
+            <div className="mt-4 flex flex-col gap-2 lg:mt-0">
+              <p className="text-[16px] font-normal text-gray-700 text-center lg:text-left">
+                Join us on this journey of growth
+              </p>
+              <div className="flex max-w-xs mx-auto w-full lg:mx-0 lg:max-w-sm">
+                <Input
+                  className="rounded-r-none active:outline-none"
+                  placeholder="Email address"
+                />
+                <Button variant="default" className="rounded-l-none">
+                  Join Club
+                </Button>
+              </div>
             </div>
           </section>
-          <section className="flex justify-center items-center">
-            <Image src="/assets/HomePage.png" alt="gdsc-logo" width="450" height="450" />
+          <section className="flex justify-center items-center animate-levitate">
+            <Image
+              src="/assets/dev.png"
+              alt="gdsc-logo"
+              width="350"
+              height="350"
+            />
           </section>
         </section>
       </MaxWidthWrapper>
@@ -29,5 +47,5 @@ export default async function Page() {
         <Teams />
       </main>
     </>
-  )
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -67,14 +67,20 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        levitate: {
+          "0%": { transform: "transform(0,0px)" },
+          "50%": { transform: "transform(0,15px)" },
+          "100%": { transform: "translate(0, -0px)" },
+        },
       },
       animation: {
+        levitate: "levitate 0.2s ease-in-out infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
