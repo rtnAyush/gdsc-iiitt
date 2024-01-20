afb29923165c0fa8ab81f2cc38df720f10c712dd
Rishabh705 <rishabh0739@gmail.com>
2024-01-20 22:47:37 +0530

    Hamburger UI updates

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -8,6 +8,9 @@ import {
 } from "@/components/ui/sheet"
 import { cn } from "@/lib/utils"
 import { RxHamburgerMenu } from "react-icons/rx"
+import { FaHome,FaCalendarAlt } from "react-icons/fa";
+import { RiTeamFill } from "react-icons/ri";
+import { MdLeaderboard } from "react-icons/md";
 import Link from "next/link"
 import { buttonVariants } from "@/components/ui/button"
 
@@ -16,17 +19,29 @@ export default function NavMenu({ className }) {
         return (
             <div className={cn(className)}>
                 <Sheet>
-                    <SheetTrigger><RxHamburgerMenu size={30}/></SheetTrigger>
+                    <SheetTrigger><RxHamburgerMenu size={30} /></SheetTrigger>
                     <SheetContent>
                         <SheetHeader>
-                            <SheetTitle className="text-2xl">Menu</SheetTitle>
+                            <SheetTitle className="pl-3 text-2xl text-left">Menu</SheetTitle>
                         </SheetHeader>
-                        <div className="py-4 flex flex-col items-start space-y-4">
-                            <Link href="/" className="text-xl">Home</Link>
-                            <Link href="/events" className="text-xl">Events</Link>
-                            <Link href="/team" className="text-xl">Teams</Link>
-                            <Link href="/leaderboard" className="text-xl">Leaderboard</Link>
-                            <Link href="/" className={buttonVariants({ variant: "default" })}>Join Club</Link>
+                        <div className="py-4 flex flex-col">
+                            <Link href="/" className="flex items-center p-3 text-xl hover:bg-slate-200">
+                                <FaHome className="inline-block mr-2" size={20} />
+                                Home
+                            </Link>
+                            <Link href="/events" className="flex items-center p-3 text-xl hover:bg-slate-200">
+                                <FaCalendarAlt className="inline-block mr-2" size={20} />
+                                Events
+                            </Link>
+                            <Link href="/team" className="flex items-center p-3 text-xl hover:bg-slate-200">
+                                <RiTeamFill className="inline-block mr-2" size={20} />
+                                Teams
+                            </Link>
+                            <Link href="/leaderboard" className="flex items-center p-3 text-xl hover:bg-slate-200">
+                                <MdLeaderboard className="inline-block mr-2" size={20} />
+                                Leaderboard
+                            </Link>
+                            <Link href="/" className={`${buttonVariants({ variant: "default" })} w-28 m-4`}>Join Club</Link>
                         </div>
                     </SheetContent>
                 </Sheet>
